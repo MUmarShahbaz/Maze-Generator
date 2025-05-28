a00ef145ae848294b8045054c021723da8ebe4f8
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-05-28 18:58:13 +0500

    Updated Example file

--- example.py
+++ example.py
@@ -1,16 +1,17 @@
 from treecrypt import KeyMaker, Crypter
+import os
 
 try:
     open('keys/key.txt', 'r')
 except FileNotFoundError:
+    os.makedirs(os.path.dirname('keys/key.txt'), exist_ok=True)
     myKeyMaker = KeyMaker()
-    myKeyMaker.GenerateKey(20, 5)
+    myKeyMaker.GenerateKey(100, 5, livePrint=True)
     myKeyMaker.Export("keys/key.txt", "keys/dict.txt")
 
 
 myCrypter = Crypter()
 myCrypter.Import("keys/key.txt", "keys/dict.txt")
-
 plainText = input("Enter text to encrypt: ")
 cipher = myCrypter.Encrypt(plainText)
 print(cipher)
